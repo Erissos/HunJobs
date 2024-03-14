2c5d834fc97be8e45a46fe4361f94d3d39803d80
Adem Mertkan Durmaz <ademmertkan01@gmail.com>
2024-03-14 11:24:48 +0300

    Added kömürKarası Fix Woodcutter.java

--- src/main/java/me/erisos/hunjobs/listeners/Mining.java
+++ src/main/java/me/erisos/hunjobs/listeners/Mining.java
@@ -1,11 +1,16 @@
 package me.erisos.hunjobs.listeners;
 
 import me.erisos.hunjobs.HunJobs;
+import me.erisos.hunjobs.user.LevelController;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class Mining implements Listener {
 
@@ -21,6 +26,28 @@ public class Mining implements Listener {
         Player player = e.getPlayer();
         Block block = e.getBlock();
 
+        var itemInHand = player.getItemInHand().getType();
+        var PAxe1 = itemInHand == Material.WOODEN_PICKAXE;
+        var PAxe2 = itemInHand == Material.STONE_PICKAXE;
+        var PAxe3 = itemInHand == Material.IRON_PICKAXE;
+        var PAxe4 = itemInHand == Material.GOLDEN_PICKAXE;
+        var PAxe5 = itemInHand == Material.DIAMOND_PICKAXE;
+        var PAxe6 = itemInHand == Material.NETHERITE_PICKAXE;
+
+        if (!(PAxe1 || PAxe2 || PAxe3 || PAxe4 || PAxe5 || PAxe6)) {
+            return;
+        }
+
+        if (!(LevelController.getLevel(player, "mining") >= 1)) {
+            return;
+        }
+
+        if(block.getType().toString().contains("COAL_ORE")) {
+            Random random = new Random();
+            if (random.nextInt(100) < 5) {
+                block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.COAL, 3));
+            }
+        }
 
     }
 }
--- src/main/java/me/erisos/hunjobs/listeners/Woodcutter.java
+++ src/main/java/me/erisos/hunjobs/listeners/Woodcutter.java
@@ -32,9 +32,22 @@ public class Woodcutter implements Listener {
         Block aboveBlock = block.getRelative(BlockFace.UP);
         Block belowBlock = block.getRelative(BlockFace.DOWN);
 
+        var itemInHand = player.getItemInHand().getType();
+        var Axe1 = itemInHand == Material.WOODEN_AXE;
+        var Axe2 = itemInHand == Material.STONE_AXE;
+        var Axe3 = itemInHand == Material.IRON_AXE;
+        var Axe4 = itemInHand == Material.GOLDEN_AXE;
+        var Axe5 = itemInHand == Material.DIAMOND_AXE;
+        var Axe6 = itemInHand == Material.NETHERITE_AXE;
+
+        if (!(Axe1 || Axe2 || Axe3 || Axe4 || Axe5 || Axe6)) {
+            return;
+        }
+
         if (!(LevelController.getLevel(player, "woodcutter") >= 1)) {
             return;
         }
+
         if (block.getType().toString().contains("_LOG") && !player.isSneaking()) {
 
             if (aboveBlock.getType() == Material.OAK_LOG || aboveBlock.getType() == Material.OAK_LEAVES || belowBlock.getType() == Material.DIRT || belowBlock.getType() == Material.OAK_LOG) {
@@ -51,6 +64,22 @@ public class Woodcutter implements Listener {
     public void yenilenenDünya(BlockBreakEvent e) {
         Player player = e.getPlayer();
         Block block = e.getBlock();
+
+        var itemInHand = player.getItemInHand().getType();
+        var Axe1 = itemInHand == Material.WOODEN_AXE;
+        var Axe2 = itemInHand == Material.STONE_AXE;
+        var Axe3 = itemInHand == Material.IRON_AXE;
+        var Axe4 = itemInHand == Material.GOLDEN_AXE;
+        var Axe5 = itemInHand == Material.DIAMOND_AXE;
+        var Axe6 = itemInHand == Material.NETHERITE_AXE;
+
+        if (!(Axe1 || Axe2 || Axe3 || Axe4 || Axe5 || Axe6)) {
+            return;
+        }
+
+        if (!(LevelController.getLevel(player, "woodcutter") >= 2)) {
+            return;
+        }
 
         if (block.getType() == Material.OAK_LOG && !player.isSneaking()) {
 
@@ -71,6 +100,22 @@ public class Woodcutter implements Listener {
         Player player = e.getPlayer();
         Block block = e.getBlock();
 
+        var itemInHand = player.getItemInHand().getType();
+        var Axe1 = itemInHand == Material.WOODEN_AXE;
+        var Axe2 = itemInHand == Material.STONE_AXE;
+        var Axe3 = itemInHand == Material.IRON_AXE;
+        var Axe4 = itemInHand == Material.GOLDEN_AXE;
+        var Axe5 = itemInHand == Material.DIAMOND_AXE;
+        var Axe6 = itemInHand == Material.NETHERITE_AXE;
+
+        if (!(Axe1 || Axe2 || Axe3 || Axe4 || Axe5 || Axe6)) {
+            return;
+        }
+
+        if (!(LevelController.getLevel(player, "woodcutter") >= 3)) {
+            return;
+        }
+
         if (block.getType() == Material.OAK_LOG && !player.isSneaking()) {
             player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 30 ,0) );
         }
@@ -81,16 +126,21 @@ public class Woodcutter implements Listener {
         Player player = e.getPlayer();
         Block block = e.getBlock();
 
+        if (!(LevelController.getLevel(player, "woodcutter") >= 4)) {
+            return;
+        }
+
         var itemInHand = player.getItemInHand().getType();
         var Hoe1 = itemInHand == Material.WOODEN_HOE;
         var Hoe2 = itemInHand == Material.IRON_HOE;
-        var Hoe3 = itemInHand == Material.DIAMOND_HOE;
-        var Hoe4 = itemInHand == Material.GOLDEN_HOE;
+        var Hoe3 = itemInHand == Material.GOLDEN_HOE;
+        var Hoe4 = itemInHand == Material.DIAMOND_HOE;
         var Hoe5 = itemInHand == Material.NETHERITE_HOE;
+        var HoeS = itemInHand == Material.STONE_HOE;
         var Shears = itemInHand == Material.SHEARS;
         var hand = itemInHand == Material.AIR;
 
-        if (Hoe1 || Hoe2 || Hoe3 || Hoe4 || Hoe5 || Shears || hand) {
+        if (Hoe1 || Hoe2 || Hoe3 || Hoe4 || Hoe5 || HoeS || Shears || hand) {
             if (block.getType().toString().contains("_LEAVES")) {
                 Random random = new Random();
                 if (random.nextInt(100) < 10) {
@@ -103,6 +153,12 @@ public class Woodcutter implements Listener {
 
     @EventHandler
     public void köktenMüdahale(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        Block block = e.getBlock();
+
+        if (!(LevelController.getLevel(player, "woodcutter") >= 5)) {
+            return;
+        }
 
     }
 }
